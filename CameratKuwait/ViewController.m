fe3664dcb6226589afebc7698b6b24b7b3d8342c
osamagucCitySoc <osamarabie@Osamas-MacBook-Pro.local>
2015-06-02 11:15:14 +0300

    1- Drawing the cameras on the map.

--- CameratKuwait/ViewController.m
+++ CameratKuwait/ViewController.m
@@ -16,7 +16,7 @@
 @property (nonatomic, strong) CLLocationManager *locationManager;
 @property (nonatomic, strong) NSMutableArray *locations;
 @property (weak, nonatomic) IBOutlet MKMapView *map;
-
+@property (nonatomic, strong) NSArray* cameras;
 @end
 
 @implementation ViewController
@@ -34,6 +34,60 @@
     [self.map setShowsUserLocation:YES];
     [self.map setUserTrackingMode:MKUserTrackingModeFollow animated:YES];
     
+    
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"cameras" ofType:@"json"];
+    NSError* error2;
+    NSString* string = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:&error2];
+    string = [string stringByReplacingOccurrencesOfString:@"\\'" withString:@"'"];
+    NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
+    NSError* error;
+    self.cameras = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+    
+    for(NSDictionary* cameraDict in self.cameras)
+    {
+        CLLocationCoordinate2D location = CLLocationCoordinate2DMake([[cameraDict objectForKey:@"lat"] floatValue], [[cameraDict objectForKey:@"lng"] floatValue]);
+        MyCustomAnnotation* annotation;
+        if([[cameraDict objectForKey:@"type"] isEqualToString:@"red light camera"])
+        {
+            annotation = [[MyCustomAnnotation alloc]initWithTitle:@"كاميرة إشارة حمراء" location:location image:[UIImage imageNamed:@"Ampel.gif"]];
+        }else if([[cameraDict objectForKey:@"type"] hasPrefix:@"combination"])
+        {
+            NSString* title = @"";
+            NSString* image = @"";
+            if([[cameraDict objectForKey:@"speed"] intValue] == 50)
+            {
+                title = [NSString stringWithFormat:@"%@ %@ %@",@"كاميرة إشارة حمراء",@"و",@"سرعة 50KMPH"];
+                image = @"50Ampel.gif";
+            }else if([[cameraDict objectForKey:@"speed"] intValue] == 60)
+            {
+                title = [NSString stringWithFormat:@"%@ %@ %@",@"كاميرة إشارة حمراء",@"و",@"سرعة 60KMPH"];
+                image = @"60Ampel.gif";
+            }else if([[cameraDict objectForKey:@"speed"] intValue] == 70)
+            {
+                title = [NSString stringWithFormat:@"%@ %@ %@",@"كاميرة إشارة حمراء",@"و",@"سرعة 70KMPH"];
+                image = @"70Ampel.gif";
+            }else if([[cameraDict objectForKey:@"speed"] intValue] == 80)
+            {
+                title = [NSString stringWithFormat:@"%@ %@ %@",@"كاميرة إشارة حمراء",@"و",@"سرعة 80KMPH"];
+                image = @"80Ampel.gif";
+            }
+            annotation = [[MyCustomAnnotation alloc]initWithTitle:title location:location image:[UIImage imageNamed:image]];
+        }else
+        {
+            
+            NSString* title = [NSString stringWithFormat:@"%@ %i %@",@"كاميرة سرعة",[[cameraDict objectForKey:@"speed"] intValue],@"KMPH"];
+            
+            NSString* image = [NSString stringWithFormat:@"%i%@",[[cameraDict objectForKey:@"speed"] intValue],@"kmh.gif"];
+            
+            
+            
+            annotation = [[MyCustomAnnotation alloc]initWithTitle:title location:location image:[UIImage imageNamed:image]];
+
+        }
+        [self.map addAnnotation:annotation];
+    }
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -49,10 +103,6 @@
 {
     
     
-    
-    MyCustomAnnotation* annotation = [[MyCustomAnnotation alloc]initWithTitle:@"40kmh" location:newLocation.coordinate image:[UIImage imageNamed:@"40kmh.gif"]];
-    
-    [self.map addAnnotation:annotation];
     
     // Add another annotation to the map.
     /*MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
