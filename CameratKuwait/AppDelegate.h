9366dbf1bdcf96bcd544fd6c8681e347ddb2de8e
osamagucCitySoc <osamarabie@Osamas-MacBook-Pro.local>
2015-06-02 08:29:26 +0300

    Initial Commit

---
+++ CameratKuwait/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameratKuwait
+//
+//  Created by Osama Rabie on 6/2/15.
+//  Copyright (c) 2015 Osama Rabie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CameratKuwait/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameratKuwait
+//
+//  Created by Osama Rabie on 6/2/15.
+//  Copyright (c) 2015 Osama Rabie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
